0ab6b5305719495ab5fe81b79ea0d1dce9f0c97d
Emilio <115907759+Milo-IZ@users.noreply.github.com>
2025-09-23 14:02:44 -0600

    ahora si (espero)

--- start.ps1
+++ start.ps1
@@ -1,4 +1,4 @@
 Start-Process powershell -ArgumentList "cd backend; python manage.py runserver"
-Start-Process powershell -ArgumentList "cd frontend/frontend; npm run dev"
+Start-Process powershell -ArgumentList "cd frontend; npm run dev"
 
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
